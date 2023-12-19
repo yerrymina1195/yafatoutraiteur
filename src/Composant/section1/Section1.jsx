f1e0c4e2d11b523bf6df06c335e178b473d082c4
rama.fall <ramafall49@gmail.com>
2023-12-19 15:04:56 +0000

    debut travail

--- src/Composant/section1/Section1.jsx
+++ src/Composant/section1/Section1.jsx
@@ -5,6 +5,7 @@ import Menu from '../menu/Menu';
 export default function Section1() {
   return (
     <div className='mySection'>
+        <p>+221 77 256 64 08</p>
         < Menu />
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit aut et aspernatur qui. Voluptatum asperiores numquam, saepe officia nihil deleniti, hic facere doloremque dolorum illum perspiciatis. Amet maiores dicta debitis.</p>
     </div>
