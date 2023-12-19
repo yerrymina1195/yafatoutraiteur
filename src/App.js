8a341fff34125e4cf3b54b639e6dd957c0418cc1
rama.fall <ramafall49@gmail.com>
2023-12-19 22:23:18 +0000

    menu deja regle

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import Menu from './Composant/section1/Section1';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
-    <div className="MYsection1">
+    <div className="">
       < Menu />
     </div>
   );
--- src/Composant/section1/Section1.jsx
+++ src/Composant/section1/Section1.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import "./Section1.css";
 import Menu from '../menu/Menu';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
+ 
 
 export default function Section1() {
+  useEffect(() => {
+    AOS.init();
+  }, []); 
   return (
     <div className='mySection'>
-        <p>+221 77 256 64 08</p>
+        <p className='text-white'>+221 77 256 64 08</p>
         < Menu />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit aut et aspernatur qui. Voluptatum asperiores numquam, saepe officia nihil deleniti, hic facere doloremque dolorum illum perspiciatis. Amet maiores dicta debitis.</p>
+        <div
+        class="container position-relative text-center text-lg-start"
+        data-aos="zoom-in"
+        data-aos-delay="100"
+      >
+        <div class="row text-white mt-5 pt-5">
+          <div class="col-lg-8">
+            <h1>Bienvenue à<span className='text-warning'>Ya Fatou Traiteur</span></h1>
+            <h3>"Offrir une excellente cuisine depuis plus de 5 ans !"</h3>
+
+            <div class="btns">
+              <a href="#menu" class="btn btn-menu animated fadeInUp scrollto">Nos Menus </a>
+              <a
+                href="#book-a-table"
+                class="btn btn-book animated fadeInUp scrollto" >Passer une Commande</a>
+            </div>
+          </div>
+          <div
+            class="col-lg-4 d-flex align-items-center justify-content-center position-relative"
+            data-aos="zoom-in"
+            data-aos-delay="200"
+          >
+          <a href="assets/traiteur.mp4" class="glightbox play-btn">play<i class="bi bi-play-circle width=1040 height=1060"></i></a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
