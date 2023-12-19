d72af4b0c087dac43cf5162ec16dbac9ba70098f
rama.fall <ramafall49@gmail.com>
2023-12-19 15:02:58 +0000

    debut travail

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Menu from './Composant/section1/Section1';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="MYsection1">
+      < Menu />
     </div>
   );
 }
---
+++ src/Composant/section1/Section1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Section1.css";
+import Menu from '../menu/Menu';
+
+export default function Section1() {
+  return (
+    <div className='mySection'>
+        < Menu />
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit aut et aspernatur qui. Voluptatum asperiores numquam, saepe officia nihil deleniti, hic facere doloremque dolorum illum perspiciatis. Amet maiores dicta debitis.</p>
+    </div>
+  )
+}
